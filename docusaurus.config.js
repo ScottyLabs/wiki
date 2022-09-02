40ac8f30ce3506596623eef7ef7165f31a6bdb38
David Ethan Hwang <hdavidethan@gmail.com>
2022-09-02 17:46:11 -0400

    Update site url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "ScottyLabs Tech",
   tagline:
     "Applications and projects built by ScottyLabs for the CMU community",
-  url: "https://tech.scottylabs.org",
+  url: "https://wiki.scottylabs.org",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
